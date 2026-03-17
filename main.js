79a64a6081ead2a4e3012d16b0ab0c08350f36a9
FernandoArcangeli <fernando.arcangeli02@gmail.com>
2026-03-17 15:10:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+let bowling = {
+    giocatori: [
+        {nome: "Livio",punteggi: []},
+        {nome: "Paola",punteggi: []},
+        {nome: "Filippo",punteggi: []},
+        {nome: "Giuseppe",punteggi: []},
+    ],
+    creaPunteggio: function(){
+        this.giocatori.forEach(giocatore => {
+            for (let i = 1; i <= 10; i++) {
+                giocatore.punteggi.push(Math.floor(Math.random() * (10 - 1 + 1) + 1))
+            }    
+        });
+
+    },
+
+    nuovoGiocatore: function(nome){
+        let nuovoGiocatore = {nome: nome, punteggi: []}
+        for (let i = 1; i <= 10; i++) {
+            nuovoGiocatore.punteggi.push(Math.floor(Math.random() * (10 - 1 + 1) + 1))
+        }
+        this.giocatori.push(nuovoGiocatore)   
+    },
+
+    punteggioTotale: function(){
+        this.giocatori.forEach(giocatore => {
+            let totale = giocatore.punteggi.reduce((acc, punteggio) => acc + punteggio);
+            giocatore.totale = totale;
+            
+        });
+        this.giocatori.sort((a, b) => b.totale - a.totale);
+        
+    },
+    vincitore: function(){
+        let vincitore = this.giocatori[0];
+        console.log("Il vincitore è " + vincitore.nome + " con " + vincitore.totale + " punti");
+    },
+
+    classifica: function(){
+        console.log("Classifica:");
+        this.giocatori.forEach((giocatore, index) => {
+            console.log((index + 1) + ". " + giocatore.nome + " - " + giocatore.totale + " punti");
+        });
+},
+}
+
+
+bowling.creaPunteggio()
+bowling.nuovoGiocatore("Gianluca")
+bowling.punteggioTotale();
+bowling.vincitore()
+bowling.classifica()
